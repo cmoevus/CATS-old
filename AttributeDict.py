21b1e7761c691f213bad71f6ab24a753739c9806
Corentin Moevus <c.moevus@gmail.com>
2015-09-10 10:26:18 -0400

    Split Parameters into self and a more general AttributeDict parent class.
    The Parameters object was a mess. New AttributeDict parent class helps to keep
    things clean. The parameters object now is an AttributeDict with specific
    parameters: _position and _use_defaults, that are both involved in using
    (or not) the 'DEFAULTS' dict

---
+++ AttributeDict.py
@@ -0,0 +1,131 @@
+#!env /usr/bin/python
+# -*- coding: utf8 -*-
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+from copy import deepcopy
+
+
+class AttributeDict(object):
+    """
+    Dictionary-like object that allow to access items as an object's attributes.
+
+    AttributeDicts allow to add getters and setters on items, as well as to
+    access them through AttributeDict.key or AttributeDict['key']. It can be
+    used as a dictionary for enumerating items (as in **kwargs or in for
+    loops). It supports hidden items (keys starting with '_') that do not
+    appear in the item listings.
+
+    Recommendation on how to write getters/setters:
+    -----------------------------------------------
+    In order to keep keywords logical between the dictionary view and the
+    attribute view of the object, use self.__dict__['_attribs'][k] = v, for
+    example, instead of object.__setattr__(self, 'k', v). Indeed, the latter
+    case will lead to infinite recursion. Using a different 'transitive' name,
+    for example '_k' will lead to improper listing when iterating over the
+    values, for '_k' will be hidden, and any other value will losse integrity
+    between the dict view and the attrib view.
+
+    Arguments:
+    ----------
+    Any iterable object that can be accessed by iteritems()
+    or as a pair of (keyword, value). Acceptable arguments are:
+    - Dictionaries
+    - Objects with a iteritems() function
+    - Tuples/Lists of (keywork, value)
+    - (keyword, value) as a tuple/list
+    - keyworded arguments
+    """
+
+    fetched_functs = ['keys', 'values', 'items', 'iterkeys', 'iteritems',
+                      'itervalues', '__iter__']
+
+    def __init__(self, *args, **kwargs):
+        self.__dict__['_attribs'] = dict()
+        self.update(*args, **kwargs)
+
+    def __getattr__(self, attr):
+        """Return a fetched function, an attribute-item or an error."""
+        error = '{0} not found'.format(attr)
+        if attr in self.fetched_functs:
+            return getattr(self.__dict__['_attribs'], attr)
+        else:
+            try:
+                return self.__dict__['_attribs'][attr]
+            except KeyError:
+                raise AttributeError(error)
+
+    def __setattr__(self, attr, value):
+        """
+        Put the right attribute/item into the right dict.
+
+        Also:
+        - Prevents from overwriting fetched functions
+        - Transforms all subdicts into objects of the same type as this one
+        """
+        if attr in self.fetched_functs:
+            # Prevent from blocking the read-only attributes from __dict__
+            e = 'object attribute {0} is read-only'.format(attr)
+            raise AttributeError(e)
+        else:
+            # Transform all subdicts into same type as self
+            if type(value) is dict:
+                value = type(self)(**value)
+
+            # Put item in the right dict
+            if attr[0] == '_' or attr in dir(self):
+                object.__setattr__(self, attr, value)
+            else:
+                self.__dict__['_attribs'][attr] = value
+
+    def __repr__(self):
+        return self.__dict__['_attribs'].__repr__()
+
+    def __str__(self):
+        return self.__dict__['_attribs'].__str__()
+
+    def __setitem__(self, key, value):
+        self.__setattr__(key, value)
+
+    def __getitem__(self, key):
+        try:
+            return self.__getattribute__(key)
+        except AttributeError:
+            return self.__getattr__(key)
+
+    def __contains__(self, key):
+        if key[0] == '_':
+            return key in self.__dict__
+        else:
+            return key in self.__dict__['_attribs']
+
+    def __delattr__(self, attr):
+        try:
+            object._delattr__(self, attr)
+        except AttributeError:
+            del self.__dict__['_attribs'][attr]
+
+    def update(self, *args, **kwargs):
+        """
+        Update the object with the given list/object.
+        """
+        # Import dicts, lists and pairs
+        for d in args:
+            if 'iteritems' in dir(d) or isinstance(d, AttributeDict):
+                d = d.iteritems()
+            elif '__iter__' not in dir(d[0]):
+                d = (d, )
+            for k, v in d:
+                setattr(self, k, v)
+        # Import keywords
+        for k, v in kwargs.iteritems():
+            setattr(self, k, v)
+
+    def copy(self):
+        return type(self)(self.__dict__.copy())
+
+    def deepcopy(self):
+        d = deepcopy(self.__dict__)
+        return type(self)(d)
