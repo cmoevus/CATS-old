c943e9c41f410af722a01413b633ca99b20dbc38
Corentin Moevus <c.moevus@gmail.com>
2015-09-28 19:31:55 -0400

    Include management of channels
    Better management of image directories via the ImageDir class now allow to handle channels. Code is overall cleaner.

--- cats/sources.py
+++ cats/sources.py
@@ -40,7 +40,7 @@ class ImageDir(object):
             if type(source) is dict:
                 channels.update(source)
             else:
-                channels[name] = source
+                channels[str(name)] = source
                 name += 1
         channels.update(kwargs)
         self.channels, self.sources = list(), list()
@@ -50,44 +50,101 @@ class ImageDir(object):
             if n_files == 0:
                 raise ValueError('Source {0} is empty or does not exist.'.format(source))
             else:
-                self.channels, self.sources = name, source
+                self.channels.append(name)
+                self.sources.append(source)
         # Compatibility with bioformats.ImageReader
         self.rdr = self
 
     def getSizeX(self):
         """Return the size in the x dimension (in pixels)."""
-        return self.read(c=0, t=0).shape[1]
+        return self.read(c=0, t=0, rescale=False).shape[1]
 
     def getSizeY(self):
         """Return the size in the y dimension (in pixels)."""
-        return self.read(c=0, t=0).shape[0]
+        return self.read(c=0, t=0, rescale=False).shape[0]
+
+    def getImageCount(self):
+        """Return the number of images in series."""
+        return self.getSizeT()
 
     def getSizeT(self):
-        """Return the number of frames in series."""
-        return len(glob(self.sources[0]))
+        """Return the number of images in series."""
+        length = len(glob(self.sources[0]))
+        if length == 0:
+            raise ValueError('Source {0} is empty or does not exist.'.format(self.sources[0]))
+        else:
+            return length
 
     def getSizeC(self):
         """Return the number of channels."""
         return len(self.sources)
 
-    def read(self, c=None, t=0):
+    def getSizeZ(self):
+        """Return 1. ImageDir does not support z stacks."""
+        return 1
+
+    @property
+    def path(self):
+        """Return a dict that could be used to initiate the object."""
+        if len(self.channels) > 1 or self.channels[0] != '0':
+            return dict(zip(self.channels, self.sources))
+        else:
+            return self.sources[0]
+
+    @path.setter
+    def path(self, value):
+        """Prevent wrongdoers from using path."""
+        raise ValueError('"path" is read-only')
+
+    def read(self, c=None, t=0, rescale=True):
         """Return an image from the sources."""
         # A. Fetch the image(s)
-        c = self.channels if c is None else c
+        if c is None:
+            c = self.channels
+        elif '__iter__' not in dir(c):
+            c = [c]
         image = list()
         for channel in c:
-            if type(channel) is int:
-                source = self.sources[channel]
-            else:
-                source = self.sources[self.channels[channel]]
-        image.append(io.imread(glob(source)[t]))
+            try:
+                if type(channel) is int:
+                    source = self.sources[channel]
+                else:
+                    source = self.sources[self.channels.index(channel)]
+            except (IndexError, ValueError):
+                raise ValueError('Channel "{0}" does not exist'.format(channel))
+            try:
+                img = io.imread(glob(source)[t])
+                if rescale == True:  # This is stupid because it is relative to this image, not the series. This will change the intensity scaling factor between images. But I did not choose.
+                    img = img.astype(float) / img.max()
+                image.append(img)
+            except IndexError:
+                if os.path.exists(source) == False:
+                    e = ['Source', source]
+                else:
+                    e = ['Frame', t]
+                raise ValueError('{0} {1} does not exist'.format(*e))
+
         # B. Make a 3D array if need be.
         if len(image) > 1:
-            img = np.zeros((image[0].shape[1], image[0].shape[0], len(image)), dtype=int)
+            img = np.zeros((image[0].shape[0], image[0].shape[1], len(image)), dtype=img.dtype)
             for n, i in enumerate(image):
                 img[:, :, n] = image[n]
             image = img
+        else:
+            image = image[0]
+
         return image
+
+    def __repr__(self):
+        """Funkyprint the sources."""
+        if len(self.sources) > 1:
+            base = os.path.split(os.path.commonprefix(self.sources))[0] + '/'
+            ext = ''
+            for source in self.sources:
+                ext += source.replace(base, '') + ', '
+            return base + '[' + ext[:-2] + ']'
+        else:
+            return self.sources[0]
 
 
 @extensions.append
@@ -97,65 +154,78 @@ class Images(object):
     #
 
     """
-    Access raw images and metadata.
+    Access raw images.
 
     Images are what comes out of an experiment. It is the raw, unanalysed,
     unfiltered, untransformed set of data recorded by the camera.
+
+    This object will save information about the images so that it can be used even in the absence of the images, post pickling/unpickling. It does not save the images, only basic metadata about them.
 
     Keyword argument:
         source: the path to the file or directory of images. In the latter case, the path can contain wildcards to select specific files within the dict.
     """
 
     def __init__(self, source):
-        """Load either BioFormats or Tiff list."""
-        self.dormant = True
+        """Load either BioFormats or ImageDir image readers."""
         self.source = source
-        self._dims = None
-        self._len = None
 
     @property
     def source(self):
         """Return the path to the images."""
-        return self._source
+        if '_source' in self.__dict__:
+            return self._source
+        else:
+            raise ValueError("The source '{0}' is not accessible.".format(self.path))
 
     @source.setter
     def source(self, source):
-        """Select between a BioFormats reader or a image list reader."""
-        source = globify(source)
-        n_files = len(glob(source))
-        if n_files == 0:
-            raise ValueError('Source {0} is empty or does not exist.'.format(source))
-        elif n_files == 1:
-            self.is_file = True
-            javabridge.start_vm(class_path=bioformats.JARS)
-            self.reader = bioformats.ImageReader(source)
-        else:
-            self.is_file = False
-        self._source = source
-        self.dormant = False
+        """Set the appropriate source reader."""
+        # A. Path: bioformats or ImageDir?
+        if type(source) is str:
+            source = globify(source)
+            n_files = len(glob(source))
+            if n_files == 0:
+                raise ValueError('Source {0} is empty or does not exist.'.format(source))
+            elif n_files == 1:
+                javabridge.start_vm(class_path=bioformats.JARS)
+                self._source = bioformats.ImageReader(source)
+            else:
+                self._source = ImageDir(source)
+        # B. List or dict: ImageDir
+        elif type(source) in [tuple, set, list]:
+            self._source = ImageDir(*source)
+        elif type(source) is dict:
+            self._source = ImageDir(**source)
+        # C. Object
+        elif isinstance(source, bioformats.ImageReader) or isinstance(source, ImageDir):
+            self._source = source
+        self._save_attributes()
 
-    def check_source(self):
+    def activate(self):
         """
         Activate the source and return True if source can be read, raise error if not.
 
         When loading (unpickling) an Images object, if the source is not accessible, the object's properties still will be. However, one may want to plug the source back in on the fly. If the source suddenly becomes accesible, this function will 'activate' the object for reading images.
         """
-        if len(glob(self.source)) == 0:
-            self.dormant = True
-            raise ValueError('Source {0} is not accesible.'.format(self.source))
-        else:
-            if self.dormant == True:
-                self.source = self.source
-                self.dormant = False
+        if '_source' in self.__dict__:
             return True
+        else:
+            try:
+                self.source = self.path
+            except:
+                raise ValueError('Source is not accessible.')
 
     @property
     def dimensions(self):
         """Return the shape of the images in the format (x, y), in pixels."""
-        if self._dims is None:
-            self.check_source()
-            self._dims = io.imread(glob(self.source)[0]).shape[::-1] if self.is_file == False else (self.reader.rdr.getSizeX(), self.reader.rdr.getSizeY())
-        return self._dims
+        try:
+            self._dims = self.source.rdr.getSizeX(), self.source.rdr.getSizeY()
+        except:
+            pass
+        try:
+            return self._dims
+        except:
+            raise ValueError('Source files are not accessible.')
 
     @dimensions.setter
     def dimensions(self, value):
@@ -165,15 +235,49 @@ class Images(object):
     @property
     def length(self):
         """Return the number of frames in the experiment."""
-        if self._len is None:
-            self.check_source()
-            self._len = len(glob(self.source)) if self.is_file == False else self.reader.rdr.getSizeT()
-        return self._len
+        try:
+            # BioFormats considers Tiff stacks to be in Z. We consider them to be in T. ImageCount is independent of T or Z.
+            self._len = self.source.rdr.getImageCount()
+        except:
+            pass
+        try:
+            return self._len
+        except:
+            raise ValueError('Source files are not accessible.')
 
     @length.setter
     def length(self, value):
         """Return an error: length is read-only."""
         return AttributeError("The 'length' attribute is read-only")
+
+    @property
+    def channels(self):
+        """Return the number of channels."""
+        try:
+            self._channels = self.source.rdr.getSizeC()
+        except:
+            pass
+        try:
+            return self._channels
+        except:
+            raise ValueError('Source files are not accessible.')
+
+    @property
+    def path(self):
+        """Return the path to the source files."""
+        try:
+            self._path = self.source.path
+        except:
+            pass
+        try:
+            return self._path
+        except:
+            raise ValueError('Source files became inaccessible before I could read them...')
+
+    @path.setter
+    def path(self, value):
+        """Return an error: path is read-only."""
+        return AttributeError("The 'path' attribute is read-only. Set the source of files with the 'source' attribute.")
 
     @property
     def shape(self):
@@ -187,13 +291,14 @@ class Images(object):
 
     def read(self):
         """Iterate over the images in the dir/file, return a numpy array representing the image."""
-        self.check_source()
-        if self.is_file is False:
-            for f in sorted(glob(self.source)):
-                yield io.imread(f)
-        elif self.is_file == True:
+        self.activate()
+        if self.length == self.source.rdr.getSizeZ():
+            # BioFormats considers Tiff stacks to be in Z. We consider them to be in T.
             for t in range(self.length):
-                yield self.reader.read(t=t, rescale=False)
+                yield self.source.read(z=t, rescale=False)
+        else:
+            for t in range(self.length):
+                yield self.source.read(t=t, rescale=False)
 
     def get(self, frame):
         """
@@ -202,33 +307,31 @@ class Images(object):
         Keyword argument:
             frame: the frame to return
         """
-        self.check_source()
-        if self.is_file == False:
-            return io.imread(sorted(glob(self.source))[frame])
-        elif self.is_file == True:
-            return self.reader.read(t=frame, rescale=False)
+        self.activate()
+        if self.length == self.source.rdr.getSizeZ():
+            # BioFormats considers Tiff stacks to be in Z. We consider them to be in T.
+            return self.source.read(z=frame, rescale=False)
+        else:
+            return self.source.read(t=frame, rescale=False)
 
     def __repr__(self):
-        """Return the path to the images."""
-        return self.source
+        """Return the path of the images."""
+        return 'Images from ' + self.source.__repr__()
 
-    def __str__(self):
-        """Return the path to the images."""
-        return self.source
 
     def _save_attributes(self):
         """Put the attributes from the Images into the object, in view of pickling and unpickling without the source."""
-        try:
-            self.dimensions
-            self.length
-        except:
-            pass
+        for attr in [i for i in dir(self)]:
+            try:
+                getattr(self, attr)
+            except:
+                pass
 
     def __getstate__(self):
         """Pickle the whole object, except the reader."""
         # Put info in memory
         self._save_attributes()
-        return dict(((k, v) for k, v in self.__dict__.items() if k != 'reader'))
+        return dict(((k, v) for k, v in self.__dict__.items() if k != '_source'))
 
     def __setstate__(self, state):
         """Load the whole object, but tolerate the absence of the source."""
@@ -236,10 +339,9 @@ class Images(object):
             if k != '_source':
                 setattr(self, k, v)
         try:
-            setattr(self, 'source', state['_source'])
+            setattr(self, 'source', state['_path'])
         except:
-            self._source = state['_source']
-            self.dormant = True
+            pass
 
 
 @extensions.append
@@ -258,10 +360,10 @@ class ROI(Images):
         c: channels to use (indice of the first channel is 0). Only applies to image files. Not TIF dirs.  If None, selects all channels.
     """
 
-    def __init__(self, images, x=None, y=None, t=None, c=[0]):
+    def __init__(self, images, x=None, y=None, t=None, c=None):
             """Build the image object and set the limits."""
             self.images = images if isinstance(images, Images) is True else Images(images)
-            self.x, self.y, self.t, self.channel = x, y, t, c
+            self.x, self.y, self.t, self.c = x, y, t, c
 
     def _slicify(self, s, fallback=slice(None)):
         """
@@ -345,35 +447,31 @@ class ROI(Images):
     @c.setter
     def c(self, value):
         """Set the list of channels to select."""
+        value = range(self.images.channels) if value is None else value
+        self._c = value
 
     def read(self):
         """Return a generator of ndarrays of every images in the source."""
-        self.images.check_source()
-        if self.images.is_file is False:
-            for f in sorted(glob(self.images.source))[self.t]:
-                yield io.imread(f)[self.y, self.x]
-        elif self.images.is_file == True:
-            for t in range(self.t.start, self.t.stop):
-                yield self.images.reader.read(t=t, c=self.channel, rescale=False)[self.y, self.x]
-        else:
-            raise ValueError('Source is not accessible')
+        for t in range(self.t.start, self.t.stop):
+            i = self.images.get(t)[self.y, self.x]
+            yield i if i.ndim == 2 else i[:, :, self.c]
 
     def get(self, frame):
         """Return the given frame."""
         i = self.images.get(frame + self.t.start)[self.y, self.x]
-        return i if i.ndim == 2 else i[:, :, self.channel]
+        return i if i.ndim == 2 else i[:, :, self.c]
 
     def __repr__(self):
         """Return the path to the images with the limits."""
-        if self.images.is_file == False:
-            r = {'source': self.source, 'x': self.x, 'y': self.y, 't': self.t}.__repr__()
-        else:
-            r = {'source': self.source, 'x': self.x, 'y': self.y, 't': self.t, 'channel': self.channel}.__repr__()
-        return r
-
-    def __str__(self):
-        """Return the path to the images."""
-        return self.source
+        r = 'ROI of {0} with limits:'.format(self.source)
+        for l in ['x', 'y', 't']:
+            v = getattr(self, l)
+            r += '\n\t{0} from {1} to {2}, '.format(l, v.start, v.stop)
+        c = [self.c] if type(self.c) is int else self.c
+        r = r[:-1] + '\n\tchannel{0}'.format('s ' if len(c) > 1 else ' ')
+        for i in c:
+            r += str(i) + ', '
+        return r[:-2]
 
     def __getstate__(self):
         """Return state. Do not inherit Images.__getstate__."""
